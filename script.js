d0b2c2dc12768689f16fadc322636abd5c00307a
GuilhermeGDGP <guilhermediasgoncalvesdev@gmail.com>
2021-02-08 23:09:13 -0300

    Create Snake

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const canvas = document.getElementById('snake')
 const context = canvas.getContext('2d')
 const box = 32
+let snake = [
+    {
+        x: 8 * box,
+        y: 8 * box
+    }
+]
 
 const createBG = () => {
     context.fillStyle = 'lightgreen'
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
+const createSnake = () => {
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = 'green'
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
 createBG()
+createSnake()
